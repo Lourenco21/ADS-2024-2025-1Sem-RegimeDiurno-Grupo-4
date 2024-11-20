f3ac091cb91a39d88986d01964b0eae3f693bf16
Lourenco21 <lcfvs@iscte-iul.pt>
2024-11-20 20:55:59 +0000

    Change table to tabulator

--- HorárioManager/horariomanagerapp/urls.py
+++ HorárioManager/horariomanagerapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("horarioloaded", views.showhorario, name="horarioloaded")
+    path("", views.showhorario, name="horarioloaded")
 ]
